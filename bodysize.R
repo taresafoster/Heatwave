63d669f192b248c10aa8d38f90fbfc4cad1ab285
tgrainger <tess.grainger@uoguelph.ca>
2025-10-06 14:50:34 -0400

    Minor changes to make it run on my computer

--- bodysize.R
+++ bodysize.R
@@ -1,5 +1,5 @@
-#set your working directory - this will be different on your computer
-setwd("/Users/taresa/Documents/R stuff/TaresaHeatwave/Data")
+#TESS SAYS: remove this and use the code below to get the data in
+#setwd("/Users/taresa/Documents/R stuff/TaresaHeatwave/Data")
 
 #load the packages that I need for this code
 library(ggplot2)
@@ -19,8 +19,11 @@ theme_tess <- function () {
     theme(plot.title = element_text(hjust = 0.5,size=20))+
     theme(axis.title.y=element_text(size=20))
 }
-data <-read.csv("preheatwavebodysize.csv",stringsAsFactors = FALSE,
+
+#TESS SAYS: I changed this so that it should now open on both our computers
+data <-read.csv("./data/preheatwavebodysize.csv",stringsAsFactors = FALSE,
                 strip.white = TRUE, na.strings = c("NA",""))
+
 
 data$adapted_temp<-as.factor(data$adapted_temp) #make adapted temp into a factor so we can color code by it in the plot
 
@@ -46,21 +49,21 @@ p <- ggplot(summary_data, aes(x = adapted_temp, y = mean, color = sex)) +
               aes(x = adapted_temp, y = weight, color = sex),
               position = position_jitterdodge(jitter.width = 0, dodge.width = 0.4), 
               size = 3, alpha = 0.5) +
-  
   geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
                 position = position_dodge(width = 0.4),
                 width = 0) +
-  
   scale_color_manual(
     values = c("f" = "palevioletred", "m" = "steelblue3"),   # or whatever colors you like
     labels = c("f" = "Female", "m" = "Male"),
-    name = "Sex"
-  ) +
-  
+    name = "Sex")+
   xlab("Adapted temperature") +
   ylab("Body size (g)") +
-  scale_x_discrete (breaks = c("25", "30", "35"), labels = c("25°C", "30°C", "35°C")) 
+  scale_x_discrete (breaks = c("25", "30", "35"), labels = c("25°C", "30°C", "35°C"))+
+  theme_tess()
+
+windows();p #TESS SAYS: if you have a pc this opens the plot in an actual new window
 
 print(p) #this opens the plot in a new window
-ggsave(file="Figures/Taresabodysizefigure.pdf", p, width = 22, height = 22, units = "cm") #close the file before you save
+ggsave(file="Figures/Taresabodysizefigure.pdf", p, width = 22, 
+       height = 22, units = "cm") #close the file before you save
 
--- propmortality.R
+++ propmortality.R
@@ -1,5 +1,6 @@
+#TESS SAYS: remove this and use the code below to get the data in
 #set your working directory - this will be different on your computer
-setwd("/Users/taresa/Documents/R stuff/TaresaHeatwave/Data")
+#setwd("/Users/taresa/Documents/R stuff/TaresaHeatwave/Data")
 
 #load the packages that I need for this code
 library(ggplot2)
@@ -22,7 +23,8 @@ theme_tess <- function () {
 }
 
 #get data in
-data <-read.csv("mortalitywk2.csv",stringsAsFactors = FALSE,
+#TESS SAYS: I changed this so that it should now open on both our computers
+data <-read.csv("./data/mortalitywk2.csv",stringsAsFactors = FALSE,
                 strip.white = TRUE, na.strings = c("NA","") )
 
 data$prop_mortality<-data$mortality/data$starting_population
@@ -33,6 +35,7 @@ data2<-data %>% #create a new dataframe
   group_by(adapted_temp,population_id) %>% #specify that the groups are 
   mutate(cumulativemortality = cumsum(mortality))%>%
   mutate(cumpropmortality = cumulativemortality/starting_population)#add a new column that is now the cumulative mortality
+
 is.factor(data$weeks_since_heatwave)
 
 str(data2) #to look at your data
@@ -56,33 +59,33 @@ data2$weeks_since_heatwave <- factor(data2$weeks_since_heatwave, levels = c(0, 2
 
 p<-ggplot(summary_data, aes(x = weeks_since_heatwave, y = mean, color=adapted_temp)) +
   geom_point(size = 4,position = position_dodge(width = 0.6)) + #mean points
-  
   geom_jitter(data = data2,
               aes(x = weeks_since_heatwave, y = cumpropmortality, color = adapted_temp),
               position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.8), 
               size = 3, alpha = 0.5) +
   #geom_beeswarm(data = data2, aes(x = weeks_since_heatwave, y = cumpropmortality, color = adapted_temp),
    #             dodge.width = 0.6, size = 3, alpha = 0.5, cex = 2.7) +
-  
   scale_color_manual(values = c("cornflowerblue", "darkorange", "brown3"), #values tells it what actual colors to put  on
                      name = "Adapted temp", #name in the legend
                      labels=c("25C", "30C", "35C"), #labels in the legend
                      breaks = c("25","30","35"), #the labels as they are written in the dataframe
                      guide = guide_legend(reverse=TRUE))+
-  
   geom_errorbar(aes(ymin = mean - se, ymax = mean + se), 
                 position = position_dodge(width = 0.6),
                 width = 0) +
-  
   xlab("Weeks since heatwave") +
   ylab("Proportion mortality")+
   scale_x_discrete(breaks = c("0", "2"), labels = c("0", "2")) + #(limits = c(-1, 3), #minimum and maximum 
                   #breaks = c(0,2),#this you'll have to add more when there are more counts
                    #labels = c("0","2"))+ #this you'll have to add more when there are more counts
   theme_tess()
+
+windows();p #for Tess to open plot
+
 quartz()         # Opens a new plotting window
 plot(1:10)       # Your plot appears in that window
 print(p) #this opens the plot in a new window
 
-ggsave(file="Figures/Taresapropmortalityfigure.pdf", p, width = 22, height = 22, units = "cm") #close the file before you save
+ggsave(file="Figures/Taresapropmortalityfigure.pdf", p, width = 22, 
+       height = 22, units = "cm") #close the file before you save
 
