eeae54ac19507f8a7771ad1588ae734a9914ea59
taresafoster <taresafoster@gmail.com>
2025-10-08 15:58:21 -0400

    Added ANOVAs

--- bodysize.R
+++ bodysize.R
@@ -1,11 +1,10 @@
-#TESS SAYS: remove this and use the code below to get the data in
-#setwd("/Users/taresa/Documents/R stuff/TaresaHeatwave/Data")
 
 #load the packages that I need for this code
 library(ggplot2)
 library(tidyverse)
 library(cowplot)
 library(ggpubr)
+library(dplyr)
 
 #Tess's ggplot theme
 theme_tess <- function () { 
@@ -20,17 +19,17 @@ theme_tess <- function () {
     theme(axis.title.y=element_text(size=20))
 }
 
-#TESS SAYS: I changed this so that it should now open on both our computers
 data <-read.csv("./data/preheatwavebodysize.csv",stringsAsFactors = FALSE,
                 strip.white = TRUE, na.strings = c("NA",""))
 
+####MAIN PLOT
 
-data$adapted_temp<-as.factor(data$adapted_temp) #make adapted temp into a factor so we can color code by it in the plot
+#make adapted_temp a factor
+data$adapted_temp <- factor(data$adapted_temp,
+                            levels = c(25, 30, 35),
+                            labels = c("25°C", "30°C", "35°C"))
 
-
-##MAIN PLOT
-
-# Then calculate summary stats only on the filtered data
+#calculate summary stats
 summary_data <- data %>%
   group_by(adapted_temp, sex) %>%
   summarise(mean = mean(weight),
@@ -41,29 +40,48 @@ summary_data <- data %>%
 summary_data$group_id <- interaction(summary_data$adapted_temp, summary_data$sex)
 data$group_id <- interaction(data$adapted_temp, data$sex)
 
-
+#create plot
 p <- ggplot(summary_data, aes(x = adapted_temp, y = mean, color = sex)) +
   geom_point(size = 4, position = position_dodge(width = 0.4)) +
-  scale_x_discrete(expand = c(0.9, 2)) +
+  
+  #jitter the points
   geom_jitter(data = data,
               aes(x = adapted_temp, y = weight, color = sex),
               position = position_jitterdodge(jitter.width = 0, dodge.width = 0.4), 
               size = 3, alpha = 0.5) +
+  
+  #add error bars
   geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
                 position = position_dodge(width = 0.4),
                 width = 0) +
+  
+  #assign colours to sex
   scale_color_manual(
     values = c("f" = "palevioletred", "m" = "steelblue3"),   # or whatever colors you like
     labels = c("f" = "Female", "m" = "Male"),
     name = "Sex")+
+  
+  #assign x- and y-axis labels
   xlab("Adapted temperature") +
   ylab("Body size (g)") +
-  scale_x_discrete (breaks = c("25", "30", "35"), labels = c("25°C", "30°C", "35°C"))+
+  
+  #assign x-axis scale
+  scale_x_discrete (breaks = c("25°C", "30°C", "35°C"), labels = c("25°C", "30°C", "35°C"))+ 
   theme_tess()
 
-windows();p #TESS SAYS: if you have a pc this opens the plot in an actual new window
+windows();p #enables plot to open on a PC
 
 print(p) #this opens the plot in a new window
-ggsave(file="Figures/Taresabodysizefigure.pdf", p, width = 22, 
-       height = 22, units = "cm") #close the file before you save
 
+#saves the plot
+ggsave(file="Figures/Taresabodysizefigure.pdf", p, width = 22, height = 22, units = "cm") #close the file before you save
+
+####ANOVA
+
+  #TWO-way ANOVA
+  bodysizeanova <- aov(weight ~ adapted_temp * sex, data = data)
+  
+  summary(bodysizeanova)
+  
+  #Tukey test
+  TukeyHSD(bodysizeanova)
--- popsizeheatwave.R
+++ popsizeheatwave.R
@@ -1,12 +1,10 @@
-#TESS SAYS: remove this and use the code below to get the data in
-#set your working directory - this will be different on your computer
-#setwd("/Users/taresa/Documents/R stuff/TaresaHeatwave/Data")
 
 #load the packages that I need for this code
 library(ggplot2)
 library(tidyverse)
 library(cowplot)
 library(ggpubr)
+library(dplyr)
 
 #Tess's ggplot theme
 theme_tess <- function () { 
@@ -22,68 +20,154 @@ theme_tess <- function () {
 }
 
 #get data in
-#TESS SAYS: I changed this so that it should now open on both our computers
 data <-read.csv("./data/heatwavepopulationwk12.csv",stringsAsFactors = FALSE,
                 strip.white = TRUE, na.strings = c("NA",""))
+ 
+#### MAIN PLOT
 
-data$heatwave <- factor(data$heatwave, levels = c(0, 1))
+#make heatwave and adpated_temp factors
+  data$heatwave <- factor(data$heatwave, levels = c(0, 1))
+  
+  data$adapted_temp<-as.factor(data$adapted_temp) #make adapted temp into a factor so we can color code by it in the plot
+  
+ 
+ # controls <- data %>%
+ #   filter(heatwave == "0", weeks_since_heatwave %in% c("0", "2", "6", "12"))
+  
+  #calculate summary stats
+  summary_data <- data %>%
+    group_by(adapted_temp, heatwave, weeks_since_heatwave) %>%
+    summarise(mean = mean(alive),
+              n=n(),
+              sd = sd(alive),
+              se = sd / sqrt(n))
+  
+  summary_data$heatwave <- factor(summary_data$heatwave, levels = c(0, 1))
+  summary_data$group_id <- interaction(summary_data$adapted_temp, summary_data$heatwave)
+  data$group_id <- interaction(data$adapted_temp, data$heatwave)
+  
+  # Convert weeks_since_heatwave to a factor with levels
+  data$weeks_since_heatwave <- factor(data$weeks_since_heatwave, 
+                                      levels = c( "0", "pad2", "2", "pad3", "6", "pad6", "12"))
+  
+  summary_data$weeks_since_heatwave <- factor(summary_data$weeks_since_heatwave, 
+                                              levels = c( "0", "pad2", "2", "pad3", "6", "pad6", "12"))
+  
+  #create plot
+  p <- ggplot(summary_data, aes(x = weeks_since_heatwave, y = mean, color = adapted_temp, shape = heatwave, group = group_id)) +
+    geom_point(size = 4, position = position_dodge(width = 0.8)) +
+    scale_x_discrete(expand = c(0.9, 2)) +
+    
+    #jitter points 
+    geom_jitter(data = data,
+                aes(x = weeks_since_heatwave, y = alive, color = adapted_temp, shape = heatwave, group = group_id),
+                position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.8), 
+                size = 3, alpha = 0.5) +
+    
+    #add error bars
+    geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                  position = position_dodge(width = 0.8),
+                  width = 0) +
+    
+    #assign shapes to heatwave
+    scale_shape_manual(values = c("0" = 16, "1" = 17),  
+                       name = "Heatwave",
+                       labels = c("0" = "Control", "1" = "Treatment")) +
+    
+    #assign colours to adapted_temp
+    scale_color_manual(values = c("cornflowerblue", "darkorange", "brown3"),
+                       name = "Adapted temperature",
+                       labels = c("25°C", "30°C", "35°C"),
+                       breaks = c("25", "30", "35"),
+                       guide = guide_legend(reverse = TRUE)) +
+    
+    #add x- and y-axis labels
+    xlab("Weeks since heatwave") +
+    ylab("Population size") +
+    scale_x_discrete (breaks = c("0", "2", "6", "12"), labels = c("0", "2", "6", "12")) +
+    theme_tess()
+  
+  quartz()         # Opens a new plotting window
+  plot(1:10)       # Your plot appears in that window
+  print(p) #this opens the plot in a new window
+  
+  windows();p #enables plot to open on a PC
 
-data$adapted_temp<-as.factor(data$adapted_temp) #make adapted temp into a factor so we can color code by it in the plot
+#saves the plot
+ggsave(file="Figures/Taresapopsizefigure.pdf", p, width = 22, height = 22, units = "cm") #close the file before you save
 
 
-##MAIN PLOT
+#### ANOVAs
 
-#TESS SAYS: changed the name of this dataframe to be descriptive so we know what it is
-#filter to just the controls
-controls <- data %>%
-  filter(heatwave == "0", weeks_since_heatwave %in% c("0", "2", "6", "12"))
+#TWO-way ANOVAs
+  #Week 0
+ week0data <- data %>%
+  filter(weeks_since_heatwave == "0")
+  
+ week0anova <- aov(alive ~ adapted_temp * heatwave, data = week0data)
+  
+ summary(week0anova)
 
-# Then calculate summary stats
-summary_data <- data %>%
-  group_by(adapted_temp, heatwave, weeks_since_heatwave) %>%
-  summarise(mean = mean(alive),
-            n=n(),
-            sd = sd(alive),
-            se = sd / sqrt(n))
+  #Week 2
+ week2data <- data %>%
+    filter(weeks_since_heatwave == "2")
+  
+ week2anova <- aov(alive ~ adapted_temp * heatwave, data = week2data)
+  
+ summary(week2anova)
+  
+  #Week 6
+ week6data <- data %>%
+    filter(weeks_since_heatwave == "6")
+  
+ week6anova <- aov(alive ~ adapted_temp * heatwave, data = week6data)
+  
+ summary(week6anova)
+  
+   #Week 12
+ week12data <- data %>%
+    filter(weeks_since_heatwave == "0")
+  
+ week12anova <- aov(alive ~ adapted_temp * heatwave, data = week12data)
+  
+ summary(week12anova)
 
-summary_data$heatwave <- factor(summary_data$heatwave, levels = c(0, 1))
-summary_data$group_id <- interaction(summary_data$adapted_temp, summary_data$heatwave)
-data$group_id <- interaction(data$adapted_temp, data$heatwave)
+#ONE-way ANOVAs - only treatment data 
+  
+  #Week 0
+ t_week0data <- data %>%
+    filter(weeks_since_heatwave == "0",
+          heatwave == "1")
+ 
+ t_week0anova <- aov(alive ~ adapted_temp, data = t_week0data)
 
-# Convert weeks_since_heatwave to a factor with levels
-data$weeks_since_heatwave <- factor(data$weeks_since_heatwave, 
-                                    levels = c( "0", "pad2", "2", "pad3", "6", "pad6", "12"))
+ summary(t_week0anova)
+ 
+  #Week 2
+ t_week2data <- data %>%
+   filter(weeks_since_heatwave == "2",
+          heatwave == "0")
+ 
+ t_week2anova <- aov(alive ~ adapted_temp, data = t_week2data)
+ 
+ summary(t_week2anova)
+ 
+  #Week 6
+ t_week6data <- data %>%
+   filter(weeks_since_heatwave == "6",
+          heatwave == "0")
+ 
+ t_week6anova <- aov(alive ~ adapted_temp, data = t_week6data)
+ 
+ summary(t_week6anova)
+ 
+  #Week 12
+ t_week12data <- data %>%
+   filter(weeks_since_heatwave == "12",
+          heatwave == "0")
+ 
+ t_week12anova <- aov(alive ~ adapted_temp, data = t_week12data)
 
-summary_data$weeks_since_heatwave <- factor(summary_data$weeks_since_heatwave, 
-                                            levels = c( "0", "pad2", "2", "pad3", "6", "pad6", "12"))
-
-p <- ggplot(summary_data, aes(x = weeks_since_heatwave, y = mean, color = adapted_temp, shape = heatwave, group = group_id)) +
-  geom_point(size = 4, position = position_dodge(width = 0.8)) +
-  #scale_x_discrete(expand = c(0.9, 2)) +
-  geom_jitter(data = data,
-              aes(x = weeks_since_heatwave, y = alive, color = adapted_temp, shape = heatwave, group = group_id),
-              position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.8), 
-              size = 3, alpha = 0.5) +
-  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
-                position = position_dodge(width = 0.8),
-                width = 0) +
-  scale_shape_manual(values = c("0" = 16, "1" = 17),  
-                     name = "Heatwave",
-                     labels = c("0" = "Control", "1" = "Treatment")) +
-  scale_color_manual(values = c("cornflowerblue", "darkorange", "brown3"),
-                     name = "Adapted temperature",
-                     labels = c("25°C", "30°C", "35°C"),
-                     breaks = c("25", "30", "35"),
-                     guide = guide_legend(reverse = TRUE)) +
-  xlab("Weeks since heatwave") +
-  ylab("Population size") +
-  scale_x_discrete (breaks = c("0", "2", "6", "12"), labels = c("0", "2", "6", "12")) +
-  theme_tess()
-
-quartz()         # Opens a new plotting window
-plot(1:10)       # Your plot appears in that window
-print(p) #this opens the plot in a new window
-
-windows();p
-
-ggsave(file="Figures/Taresapopusizefigure.pdf", p, width = 22, height = 22, units = "cm") #close the file before you save
+  summary(t_week12anova)
+ 
+  
